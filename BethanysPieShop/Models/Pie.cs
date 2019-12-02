9f4dcfdaa25d5eb846225d0c602744660e8a8992
Navid Shiraj <navid.shiraj@firstactuarial.co.uk>
2019-12-02 15:41:16 +0000

    add more iterface and magagers

--- BethanysPieShop/Models/Pie.cs
+++ BethanysPieShop/Models/Pie.cs
@@ -14,7 +14,7 @@ namespace BethanysPieShop.Models
         public string AllergyInformation { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
-        public string ImpageThumbnailUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
         public bool IsPieOfTheWeek { get; set; }
         public bool InStock { get; set; }
         public int CategoryId { get; set; }
