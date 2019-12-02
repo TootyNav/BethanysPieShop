50d2127e5035eb3a4593b7f88ec252f59402c9fc
Navid Shiraj <navid.shiraj@firstactuarial.co.uk>
2019-12-02 16:36:41 +0000

    add views and layout. Add bootstrap and jquery and images

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -35,7 +35,7 @@ namespace BethanysPieShop
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id}"
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
                     );
             });
         }
