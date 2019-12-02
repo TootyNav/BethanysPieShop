72baab53a431a98e2c4db8f7793828cc93445b8d
Navid Shiraj <navid.shiraj@firstactuarial.co.uk>
2019-12-02 16:46:35 +0000

    add css and injections

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using BethanysPieShop.Models;
 
 namespace BethanysPieShop
 {
@@ -17,6 +18,9 @@ namespace BethanysPieShop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+            services.AddScoped<IPieRepository, MockPieRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
