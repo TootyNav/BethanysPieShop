20b134b9e1263cb6397c41a912c6e681e4e4bb20
Navid Shiraj <navid.shiraj@firstactuarial.co.uk>
2019-12-05 21:55:59 +0000

    add entity framework in

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -8,19 +8,34 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using BethanysPieShop.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace BethanysPieShop
 {
     public class Startup
     {
+        /// <summary>
+        /// Use this variable to get the config file or anything else from the json file
+        /// </summary>
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            ///set up DbContext and its also a dependancy injection setup
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
 
-            services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-            services.AddScoped<IPieRepository, MockPieRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IPieRepository, PieRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
